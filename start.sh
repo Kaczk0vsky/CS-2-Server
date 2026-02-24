c30a5d444d4df9bb1547561345808ceb551f0835
Kaczk0vsky <piterspp1998@gmail.com>
2026-02-24 16:19:40 +0100

    fix: removed useless files

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ if [ -z "$STEAMUSER" ] || [ -z "$STEAMPASSWORD" ]; then
   exit 1
 fi
 
-/home/steam/steamcmd/steamcmd.sh +login "$STEAMUSER" "$STEAMPASSWORD" "$STEAMAUTHCODE" \
+/home/steam/steamcmd/steamcmd.sh +login "$STEAMUSER" "$STEAMPASSWORD" \
     +force_install_dir /home/steam/cs2-dedicated \
     +app_update 730 validate +quit
 
@@ -18,7 +18,7 @@ cd /home/steam/cs2-dedicated/game/bin/linuxsteamrt64
 
 ./cs2 -dedicated \
   -usercon \
-  -port ${CS2_PORT} \
+  -port 27015 \
   +sv_setsteamaccount ${SRCDS_TOKEN} \
-  +map ${CS2_STARTMAP} \
-  +maxplayers ${CS2_MAXPLAYERS}
+  +map de_inferno \
+  +maxplayers 32
